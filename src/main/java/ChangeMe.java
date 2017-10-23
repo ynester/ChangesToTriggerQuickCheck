a8bd908ef638343b0e79941b36efb65e3d9a64ef
ynester <ynester@ntent.com>
2017-10-23 14:39:48 -0700

    Second commit

--- src/main/java/ChangeMe.java
+++ src/main/java/ChangeMe.java
@@ -1,7 +1,7 @@
 public class ChangeMe {
     public static void main(String[] args) {
         int a=10;
-        int b=2;
+        int b=21;
         System.out.println("Here is the result: "+a+b);
     }
 }
