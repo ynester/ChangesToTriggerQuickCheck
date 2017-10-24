28848eeff23debd676d02b8b7c63e1c714677911
ynester <ynester@ntent.com>
2017-10-24 16:20:49 -0700

    BlahBlahл

--- src/main/java/ChangeMe.java
+++ src/main/java/ChangeMe.java
@@ -2,7 +2,6 @@ public class ChangeMe {
     public static void main(String[] args) {
         int a=10;
         int b=2;
-        System.out.println("Changes");
         System.out.println("Here is the result: "+a+b);
     }
 }
