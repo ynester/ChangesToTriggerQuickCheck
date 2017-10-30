c910cfd610e9b0369bdd2e581f916c0372139726
ynester <ynester@ntent.com>
2017-10-30 13:36:56 -0700

    BlahBlahл

--- src/main/java/ChangeMe.java
+++ src/main/java/ChangeMe.java
@@ -2,6 +2,6 @@ public class ChangeMe {
     public static void main(String[] args) {
         int a=12;
         int b=12;
-        System.out.println("Here is the result: "+a+b);
+        System.out.println("Here is the result: "+ a+b);
     }
 }
